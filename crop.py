180864fe7d1423aeb2ca1ad2177e37fdc6e0e420
Zhiwei Yang <zyang12@login-02.cm.cluster>
2024-04-24 16:20:10 +0000

    adding new augmented images

--- crop.py
+++ crop.py
@@ -1,55 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 23 12:07:03 2024
-
-@author: jyen
-"""
-import json
-from PIL import Image
-import glob
-import tensorflow as tf
-#from tqdm import tqdm
-import numpy as np
-import fnmatch
-#import torch
-#import torch.nn as nn
-#from torch.utils.data import DataLoader
-
-#import clip
-#from transformers import CLIPProcessor, CLIPModel
 import os
-from os import listdir
-from skimage import io
-#from sklearn.model_selection import train_test_split
-#from transformers import CLIPConfig, CLIPModel
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras.preprocessing.image import save_img
 
+input_dir = '/home/zyang12/hw/MLfinal/ML541CLIP/val2017'
+output_dir = '/home/zyang12/hw/MLfinal/ML541CLIP/images_augmented'
 
-#path to json file and folder with images
-json_path = os.path.expanduser('~/Desktop/deep_learning/final-proj/captions_val2017.json')
-image_path = os.path.expanduser('~/Desktop/deep_learning/final-proj/val2017_copy')
-filedir = os.path.expanduser('~/Desktop/deep_learning/final-proj/crop50')
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-input_data=[]
-for images in os.listdir(image_path):
-    obj = os.path.abspath(images)
-    input_data.append(obj)
-    
+datagen = ImageDataGenerator(
+    rotation_range=25,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    zoom_range=0.3,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
 
-def random_crop(image, scale):
-    width_orig = image.shape[0]
-    height_orig = image.shape[1]
-    NEW_IMG_WIDTH = width_orig*scale
-    NEW_IMG_HEIGHT = height_orig*scale
-    cropped_image = tf.image.random_crop(
-      image, size=[NEW_IMG_HEIGHT, NEW_IMG_WIDTH, 3])
-    cropped_image = np.resize(image, [width_orig, height_orig])
-    with open(filedir, "wb") as fp:
-            fp.write(cropped_image)
-            
-if __name__ == "__main__":
-    print(input_data)
-    for f in input_data:
-        io.imshow(f)
-        random_crop(f, .50)
+for filename in os.listdir(input_dir):
+    if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+        img_path = os.path.join(input_dir, filename)
+        image = load_img(img_path)
+        image = img_to_array(image)
+        image = image.reshape((1,) + image.shape)
         
+        prefix = os.path.splitext(filename)[0]
+
+        i = 0
+        for batch in datagen.flow(image, batch_size=1, save_to_dir=output_dir, save_prefix=prefix, save_format='jpeg'):
+            i += 1
+            if i > 20:
+                break
+
