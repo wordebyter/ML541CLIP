131cfff619dcdecb77d3be671bba5669284fbc42
Zhiwei Yang <zyang12@login-02.cm.cluster>
2024-04-24 18:40:46 +0000

    generate pic .sh

---
+++ cpurun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1               
+#SBATCH -n 20              
+#SBATCH --mem=30g          
+#SBATCH -J "ML541-final"   
+#SBATCH -p short           
+#SBATCH -t 12:00:00        
+#SBATCH -C EPYC-7543      
+
+module load python/3.12.3/mftt2ua
+module load cuda11.7/toolkit/11.7.1
+
+# Create a virtual environment in a directory named 'env'
+python -m venv env
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Install required Python packages from requirements.txt
+pip install -r requirements.txt
+
+# Execute the Python script
+python3 crop.py
